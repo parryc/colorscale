a2e3146bad69594f0fb8e237a075df4c37a0c0ed
parryc <parrycadwallader@gmail.com>
2013-06-07 01:07:49 -0500

    Basic color scale picking. RGB(a) done

---
+++ colorscale.js
@@ -0,0 +1,58 @@
+
+(function (root){
+	function colorscale(){
+		return {
+			data: [],
+			options: {output: 'rgb'},
+			init: function(data){
+				this.data = data;
+			},
+			interp: function(c1,c2,per){
+				var r = c1.r + (per*(c2.r-c1.r)),
+					g = c1.g + (per*(c2.g-c1.g)),
+					b = c1.b + (per*(c2.b-c1.b)),
+					a = c1.a + (per*(c2.a-c1.a));
+				return this.output({r:r,b:b,g:g,a:a});
+			},
+			output: function(color){
+				var output = this.options.output;
+				if(!output || output === 'rgb') {
+					if(color.a)
+						return 'rgba('+color.r+','+color.g+','+color.b+','+color.a+')';
+					else
+						return 'rgb('+color.r+','+color.g+','+color.b+')';
+				}
+				if(output === 'hex') {
+					return '#'+parseInt(color.r,16)+parseInt(color.g,16)+parseInt(color.b,16);
+				}
+			},
+			pick: function(point){
+				var data = this.data,
+					min = data[0].value,
+					minColor = data[0].color,
+					max = data[1].value,
+					maxColor = data[1].color,
+					windowMin, windowMax;
+				//Find which two colors to interpolate between
+				for(var i = 0; i < data.length-1; i++){
+					windowMin = data[i].value;
+					windowMax = data[i+1].value;
+
+					if(windowMin <= point <= windowMax) {
+						min = data[i].value;
+						minColor = data[i].color;
+						max = data[i+1].value;
+						maxColor = data[i+1].color;
+					}
+				}
+				return this.interp(minColor,maxColor,((point-min)/(max-min)));
+			},
+			setOutput: function(output){
+				this.options.output = output;
+			}
+		};
+	}
+
+	//export to window
+	root.colorscale = colorscale();
+})(this);
